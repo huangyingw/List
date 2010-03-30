2c4bec6d414794a32c06d5dd26bf8de02b6ba858
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-30 15:41:26 +0800

    n

--- List/List.cpp
+++ List/List.cpp
@@ -8,19 +8,6 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	List list1,list2;
-	//cin>>list1;
-	list1.AftInsert('4'-48);
-	list1.AftInsert('3'-48);
-	list1.AftInsert('2'-48);
-	list1.AftInsert('1'-48);
-	/*cin>>list2;
-	cout<<MergeCreateNew(list1,list2);
-	cout<<endl;
-	MergeList(list1,list2);
-	
-	*/
-	list1.first=list1.RevListWithRecursion(list1.first);
-	cout<<list1<<endl;
 	return 0;
 }
 
