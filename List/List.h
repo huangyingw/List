e582ad814f1f03bcf428f42971ce1e77e87bd913
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-30 16:43:17 +0800

    n

--- List/List.h
+++ List/List.h
@@ -2,25 +2,10 @@
 using namespace std;
 
 class List;
-class ListNode
-{
-	friend class List;
-	public:
-		ListNode();
-		ListNode(const int& item);
-	private:
-		int data;
-		ListNode* next;
-};
 
 class List
 {
 	public:
-		ListNode*first,*last;
-        List(){last=first=NULL;}
-		List(const int value){last=first=new ListNode(value);}
-		
-		
 		List MergeCreateNew();
 		
 		int*Remove(int i);
@@ -32,10 +17,6 @@ class List
 
 List List::MergeCreateNew()
 {
-	List result(8);
+	List result;
 	return result;
 }
-
-ListNode::ListNode():next(NULL){}
-
-ListNode::ListNode(const int& item):data(item),next(NULL){}
