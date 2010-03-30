4215ecbb44ba1c172d11d8342ea912b11647afdc
huangyingw@huangyingw-01.mscnc <huangyingw@gmail.com>
2010-03-30 16:43:39 +0800

    n

--- List/List.h
+++ List/List.h
@@ -1,18 +1,12 @@
 #include <fstream>
 using namespace std;
 
-class List;
-
 class List
 {
 	public:
 		List MergeCreateNew();
-		
-		int*Remove(int i);
-		int*Get(int i);
 	private:
 		ofstream fout; 	
-		
 };
 
 List List::MergeCreateNew()
