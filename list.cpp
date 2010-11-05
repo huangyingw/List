cd9a129db8ffcae716280bdf1e477dc24dee12cc
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-05 16:46:50 +0800

    n

--- list.cpp
+++ list.cpp
@@ -7,23 +7,23 @@
 
 int main()
 {
-	List list1,list2;
-	
-	//cin>>list1;
-	list1.InsertAtLast('4'-48);
-	list1.InsertAtLast('3'-48);
-	list1.InsertAtLast('2'-48);
-	list1.InsertAtLast('1'-48);
-	
-	cout<<list1<<endl;
-	/*
-	cin>>list2;
-	cout<<MergeCreateNew(list1,list2);
-	cout<<endl;
-	MergeList(list1,list2);
-	*/
-	list1.first=list1.RevListWithRecursion(list1.first);
-	cout<<list1<<endl;
-	return 0;
+  List list1,list2;
+
+  //cin>>list1;
+  list1.InsertAtLast('4'-48);
+  list1.InsertAtLast('3'-48);
+  list1.InsertAtLast('2'-48);
+  list1.InsertAtLast('1'-48);
+
+  cout<<list1<<endl;
+  /*
+     cin>>list2;
+     cout<<MergeCreateNew(list1,list2);
+     cout<<endl;
+     MergeList(list1,list2);
+     */
+  list1.first=list1.RevListWithRecursion(list1.first);
+  cout<<list1<<endl;
+  return 0;
 }
 
